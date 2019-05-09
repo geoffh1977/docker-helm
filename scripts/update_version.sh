c1ff6246b737d266e3dbd97e28b0d7a176ca095a
Geoffrey Harrison <geoffh1977@gmail.com>
2019-05-09 14:02:39 +1000

    Update Repo Format

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -4,14 +4,14 @@
 # Get The Versions Of The Software
 
 SITE_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-helm | grep -oP '(?<=<Key>).*?(?=</Key>)' | grep -Eo "[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}" | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n -u | tail -n1)
-LOCAL_VERSION=$(grep "finalImageVersion" container.conf | cut -d= -f 2)
+LOCAL_VERSION=$(grep "finalImageVersion" config.yml | cut -d: -f 2 | sed 's/^ //g')
 
 # Check Versions And Update File
 if [ "$SITE_VERSION" != "$LOCAL_VERSION" ]
 then
-  sed -i "s/^finalImageVersion=.*/finalImageVersion=${SITE_VERSION}/" container.conf
+  sed -i "s/^finalImageVersion:.*/finalImageVersion:\ ${SITE_VERSION}/" config.yml
   SHA=$(curl -s "https://storage.googleapis.com/kubernetes-helm/helm-v${SITE_VERSION}-linux-amd64.tar.gz.sha256" | awk '{print $1}')
-  sed -i "s/^helmSha256=.*/helmSha256=${SHA}/" container.conf
+  sed -i "s/^helmSha256:.*/helmSha256:\ ${SHA}/" config.yml
   echo " Version Updated."
 else
   echo " No Version Change."
